47190b8c053232e58a675941750c7d3f86c1c658
AndreyVerbitsky1 <s01182413@acad.tri-c.edu>
2019-01-27 19:46:29 -0500

    Add project files.

---
+++ Lab2_individualAccount(CorrectVersion)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab2_individualAccount_CorrectVersion_.MvcApplication" Language="C#" %>
---
+++ Lab2_individualAccount(CorrectVersion)/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab2_individualAccount_CorrectVersion_.Startup))]
+namespace Lab2_individualAccount_CorrectVersion_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
